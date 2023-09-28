9a528e63603e76c56b85d9ae4611261d2ad18f41
AtumaDavid <atumadavid35@gmail.com>
2023-09-28 23:54:57 +0100

    first commit

---
+++ src/app/register/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../layout/layoutContainer";
+
+export default function Register() {
+  return (
+    <Layout>
+      <Head>
+        <title>register</title>
+      </Head>
+
+      <h1>register</h1>
+    </Layout>
+  );
+}
