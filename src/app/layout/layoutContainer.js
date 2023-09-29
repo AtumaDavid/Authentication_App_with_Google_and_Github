533ec580d3873770deb99a2e21779c45ed8218b9
AtumaDavid <atumadavid35@gmail.com>
2023-09-29 18:37:09 +0100

    login page

--- src/app/layout/layoutContainer.js
+++ src/app/layout/layoutContainer.js
@@ -5,6 +5,9 @@ export default function Layout({ children }) {
   return (
     <div className="flex h-screen bg-blue-400">
       <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+        <div className={styles.imgStyle}>
+          <div></div>
+        </div>
         <div className="right flex flex-col justify-evenly">
           <div className="text-center py-10">{children}</div>
         </div>
@@ -12,3 +15,5 @@ export default function Layout({ children }) {
     </div>
   );
 }
+
+// when sign up is clicked, let the page flip like a page to the register  section
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "./layout/layoutContainer";
+import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/Form.module.css";
 
 export default function Login() {
   return (
@@ -8,7 +12,89 @@ export default function Login() {
       <Head>
         <title>login</title>
       </Head>
-      <h1>login page</h1>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+          <p className="w-3/4 mx-auto text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            officia?
+          </p>
+        </div>
+
+        <form className="flex flex-col gap-5">
+          <div className="input_group">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={styles.input_text}
+            />
+            {/* <span className="icon flex items-center px-4">
+              <HiAtSymbol size={25} />
+            </span> */}
+          </div>
+          <div className="input_group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={styles.input_text}
+            />
+            {/* <span
+              className="icon flex items-center px-4"
+              // onClick={() => setShow(!show)}
+            >
+              <HiFingerPrint size={25} />
+            </span> */}
+          </div>
+
+          <div className="input-button">
+            <div className="input-button">
+              <button type="submit" className={styles.button}>
+                Login
+              </button>
+            </div>
+            <div className="input-button">
+              <button
+                type="button"
+                // onClick={handleGoogleSignin}
+                className={styles.button_custom}
+              >
+                Sign In with Google{" "}
+                <Image
+                  src={"/assets/google.svg"}
+                  width="20"
+                  height={20}
+                  alt="google"
+                ></Image>
+              </button>
+            </div>
+
+            <div className="input-button">
+              <button
+                type="button"
+                // onClick={handleGithubSignin}
+                className={styles.button_custom}
+              >
+                Sign In with Github{" "}
+                <Image
+                  src={"/assets/github.svg"}
+                  width={25}
+                  height={25}
+                  alt="github"
+                ></Image>
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <p className="text-center text-gray-400 ">
+          {`don't`} have an account yet?
+          <Link href={"/register"} className="text-blue-700">
+            Sign Up
+          </Link>
+        </p>
+      </section>
     </Layout>
   );
 }
